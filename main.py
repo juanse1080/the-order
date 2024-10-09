391d16811b11f699bf122a266f4b582d0866b98a
Juan Marcon <juanmarcon1080@gmail.com>
2024-10-09 04:05:50 -0500

    feat: add static for deploy

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse
 
 from app.controllers import order_controller, product_controller, restaurant_controller
 from app.models.common_model import Base
 from app.db.connection import engine
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+@app.get("/")
+async def root():
+    return HTMLResponse("Hello World")
+
 app.include_router(restaurant_controller.router)
 app.include_router(product_controller.router)
 app.include_router(order_controller.router)
